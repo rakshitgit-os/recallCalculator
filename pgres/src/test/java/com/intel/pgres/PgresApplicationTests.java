22dd0da4d1ff58a55410901a83447cac547204d9
R, Rakshit <rakshit.r@intel.com>
2024-05-22 18:55:44 +0530

    initial commit

---
+++ pgres/src/test/java/com/intel/pgres/PgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intel.pgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
