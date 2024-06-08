92102491fc70b81d4fdeb3c04d1bc66194fab753
R, Rakshit <rakshit.r@intel.com>
2024-06-08 18:03:02 +0530

    test commit2

--- pgres/src/main/java/com/intel/pgres/RecallCalculatorController.java
+++ pgres/src/main/java/com/intel/pgres/RecallCalculatorController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
 
 @RestController
@@ -16,6 +21,23 @@ public class RecallCalculatorController {
 
     @GetMapping("/recall/{indexType}")
     public BigDecimal getRecall(@PathVariable("indexType") String indexType) throws SQLException, ClassNotFoundException {
-        return recallCalculator.calculateRecall(indexType);
+        BigDecimal bb=  recallCalculator.calculateRecall(indexType);
+
+        try {
+            // Now calling Files.writeString() method
+            // with path , content & standard charsets
+            Files.writeString(Path.of("/home/intel/recallValue.txt"), bb.toBigInteger().toString(),
+                    StandardCharsets.UTF_8);
+        }
+
+        // Catch block to handle the exception
+        catch (IOException ex) {
+            // Print messqage exception occurred as
+            // invalid. directory local path is passed
+            System.out.print("Invalid Path");
+        }
+
+        return  bb;
+
     }
 }
